3e970f8c549e9933c0c8b5e8a21763c36879a63a
Spiderbezno <j2agm@hotmail.com>
2019-03-15 20:33:05 +0100

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hi there! y no todo funciona correctamente
+            Hi there!new branches feature
           </p>
           <a
             className="App-link"
